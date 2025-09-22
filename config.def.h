af707daa830c0d236efc51decb2c5b7da351e5a6
Ant <marxguey@proton.me>
2025-09-22 10:04:19 -0700

    update configs to repair tags

--- config.def.h
+++ config.def.h
@@ -28,7 +28,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const char *tagsel[][2] = {
 	{ "#ffffff", "#ff0000" },
