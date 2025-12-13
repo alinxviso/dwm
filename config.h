139a6210d9896e28c73aebb05fdcd5f903df7877
Ant <marxguey@proton.me>
2025-12-12 19:18:23 -0800

    comment out dmenu colors

--- config.h
+++ config.h
@@ -88,7 +88,7 @@ static const Env envs[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-b", "-i", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-b", "-i", "-m", dmenumon, "-fn", dmenufont, /*"-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4,*/ NULL };
 //static const char *termcmd[]  = { "alacritty", "msg", "create-window", NULL };
 static const char *termcmd[] = { "urxvtc", NULL };
 static const char scratchpadname[] = "scratchpad";
